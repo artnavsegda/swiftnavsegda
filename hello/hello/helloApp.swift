5ff8a050f5c37fef3380412dfa9f98410a6a0a73
Art Navsegda <artnavsegda@gmail.com>
2021-09-10 22:53:24 +0300

    hello

---
+++ hello/hello/helloApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloApp.swift
+//  hello
+//
+//  Created by Art Navsegda on 10.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct helloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
