429a2f52155104301d64832be97b75fe38614cc0
Art Navsegda <artnavsegda@gmail.com>
2022-06-19 22:17:12 +0300

    hello

---
+++ hello/hello/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hello
+//
+//  Created by Art Navsegda on 19.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
